19a9fe50aa88eabfd372e218e27149c39d508bbb
Evgeny_Mrykhin <evgeny_mrykhin@epam.com>
2019-06-23 23:49:14 +0300

    add ssh_connect

--- go_ssh.sh
+++ go_ssh.sh
@@ -1,5 +1,1 @@
-ssh root@192.168.145.164 -p 2222 << EOF
-docker-compose -f /root/4/gradle/docker-compose.yml up
-sleep 5
-exit
-EOF
+ssh root@192.168.145.164 -p 2222 'docker-compose -f /root/4/gradle/docker-compose.yml up -d'
